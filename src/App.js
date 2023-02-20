de90b48595259367b0c4a2bbb30a6190cdc40471
Caroline-Patrick <carolinespatrick@gmail.com>
2023-02-20 15:32:17 -0600

    name and thumbnail display working

--- src/App.js
+++ src/App.js
@@ -1,18 +1,85 @@
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
-import './App.css';
+const baseUrl = 'https://randomuser.me/api?results=25';
 
-function App() {
+export default function App() {
+  const [users, setUsers] = useState(null);
+
+  useEffect(() => {
+    axios.get(baseUrl).then(response => {
+      setUsers(response.data.results);
+    });
+  }, []);
+
+  if (!users) return null;
+
   return (
-    <div className="App">
-      <header className="App-header">
-    
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      
-      </header>
+    <div>
+      {users.map(user => {
+        const { title, first, last } = user.name;
+        const {thumbnail} =user.picture
+
+        return (
+          <ul key={user.id}>
+            <li>
+              <h4>{`${title}. ${first} ${last}`}</h4>
+              <img src={thumbnail} />
+            </li>
+          </ul>
+        );
+      })}
     </div>
   );
 }
 
-export default App;
+
+// import axios from 'axios'
+// import './App.css';
+// import React, {useState, useEffect} from 'react'
+
+// const baseUrl = 'https://randomuser.me/api?results=25'
+// export default function App() {
+//   const [users, setUsers] = useState(null); 
+//   let name = {}
+//   useEffect(()=>{
+//     axios.get(baseUrl).then((response)=>{
+//       console.log(response.data.results)
+//       setUsers(response.data.results)
+//       console.log(response.data.results[0].picture.thumbnail)
+//     })
+      
+//   },[]); 
+
+//   if(!users) return null
+
+//   return (
+ 
+//       <div>
+//         {users.map((user)=>{
+//           let title =arr.name.title;
+//           let first = arr.name.first;
+//           let last = arr.name.last
+//           <ul>
+//             <li key ={users.id}>
+//               <h4>{title}. {first} {last}</h4>
+//             </li>
+            
+//           </ul>
+//        ))};
+//       </div>
+   
+//   );
+
+// }
+
+
+
+// // <ul>
+//     //   {beer.map((beer)=> (
+//     //     <li key = {beer.id}>
+//     //       <h3>{beer.name}</h3>
+//     //       <h6>{beer.tagline}</h6>
+//     //     </li>
+//     //   ))}
+//     // </ul>
